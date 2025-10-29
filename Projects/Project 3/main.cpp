453d5214ef35d4c4a31d6af36a74f2c23d2320ad
TpRedNinja <topredninja@gmail.com>
2025-10-29 11:54:14 -0400

    did some stuff for project 3

--- Projects/Project 3/main.cpp
+++ Projects/Project 3/main.cpp
@@ -3,14 +3,17 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <random>
 using namespace std;
 
 const int SIZE = 500;
+bool noWordsAvailable = false;
 
 void readFile(string sentence[]);
 string findLastWord(string userSentence);
 vector<string> FindWordInFile(string word, string sentence[]);
 vector<string> FindNextWord(string word, vector<string> validSentences);
+void display(vector<string> wordsToDisplay, string userSentence);
 
 int main()
 {
@@ -35,6 +38,9 @@ int main()
     */
 
     wordsToDisplay = FindNextWord(word, possibleSentences);
+    display(wordsToDisplay, userSentence);
+
+
 
     return 0;
 }
@@ -95,4 +101,86 @@ vector<string> FindNextWord(string word, vector<string> validSentences)
         i++;
     }
 
+    return wordsToDisplay;
+}
+
+void display(vector<string> wordsToDisplay, string userSentence)
+{
+    cout << "Your words are:" << endl;
+    cout << "-------------------" << endl;
+    int size = wordsToDisplay.size();
+    vector<string> chosenWords = {};
+    string option1, option2, option3, newSentence;
+    int choice;
+
+    random_device rd;
+    mt19937 gen(rd());
+
+    if (size == 0)
+    {
+        noWordsAvailable = true;
+        option1 = "NULL";
+        option2 = "NULL";
+        option3 = "NULL";
+        cout << option1 << endl;
+        cout << option2 << endl;
+        cout << option3 << endl;
+
+    } else if (size == 3)
+    {
+        option1 = wordsToDisplay[0];
+        option2 = wordsToDisplay[1];
+        option3 = wordsToDisplay[2];
+        cout << option1 << endl;
+        cout << option2 << endl;
+        cout << option3 << endl;
+    }
+    else if (size < 3 && size > 0)
+    {
+        if (size == 1)
+        {
+            option1 = wordsToDisplay[0];
+            option2 = "NULL";
+            option3 = "NULL";
+        } else if (size == 2)
+        {
+            option1 = wordsToDisplay[0];
+            option2 = wordsToDisplay[1];
+            option3 = "NULL";
+        }
+
+    } else if (size > 3)
+    {
+        sample(wordsToDisplay.begin(), wordsToDisplay.end(), back_inserter(chosenWords),3, gen);
+        option1 = chosenWords[0];
+        option2 = chosenWords[1];
+        option3 = chosenWords[2];
+        cout << option1 << endl;
+        cout << option2 << endl;
+        cout << option3 << endl;
+    }
+
+    while (choice > 3 || choice < 1)
+    {
+        cout << "Enter 1 for the first word 2 for the second and 3 for the third word";
+        cin >> choice;
+        if (choice > 3 || choice < 1)
+        {
+            cout << "Please enter a valid choice";
+        }
+    }
+
+    switch (choice)
+    {
+        case 1:
+            newSentence = userSentence + " " + option1;
+        case 2:
+            newSentence = userSentence + " " + option2;
+        case 3:
+            newSentence = userSentence + " " + option3;
+        default:
+            newSentence = userSentence + " " + option1;
+    }
+
+    cout <<  "Your new sentence is: " << newSentence << endl;
 }
