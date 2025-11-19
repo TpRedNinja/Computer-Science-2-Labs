e458ca044f9af1e47a46e1bdfa6c574acfc8b5a8
TpRedNinja <topredninja@gmail.com>
2025-11-19 13:20:36 -0500

    more of project 4 is done

--- Projects/Project 4/Book.cpp
+++ Projects/Project 4/Book.cpp
@@ -1,5 +1,1 @@
-//
-// Created by jjdom on 11/12/2025.
-//
-
 #include "Book.h"
--- Projects/Project 4/Book.h
+++ Projects/Project 4/Book.h
@@ -1,13 +1,29 @@
 #ifndef PROJECT_4_BOOK_H
 #define PROJECT_4_BOOK_H
-
+#include <string>
+#include <iostream>
+using namespace std;
 
 class Book
 {
     public:
-        Book();
-        ~Book();
+        Book(); // constructor no args
+        Book(int id, string title, string author, int num_of_pages, double cost, bool isCheckOut); // constructor with args
+        Book(const Book& book); // copy constructor
+        ~Book(); // deconstructor
+
+        // Functions
+        void View();
+        bool Checkout();
+        bool Checkin();
+
     private:
+        int m_id;
+        string m_title;
+        string m_author;
+        int m_num_of_pages;
+        double m_cost;
+        bool m_IsCheckOut;
 };
 
 
