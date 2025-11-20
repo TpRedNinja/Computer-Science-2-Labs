b7cc48c509b9ec653a68aabe8d8579d693bbee31
TpRedNinja <topredninja@gmail.com>
2025-11-20 14:32:17 -0500

    book.cpp work

--- Projects/Project 4/Book.cpp
+++ Projects/Project 4/Book.cpp
@@ -1,1 +1,19 @@
 #include "Book.h"
+Book::Book()
+{
+    m_id = 0;
+    m_title = "Undefined";
+    m_author = "Undefined";
+    m_num_of_pages = 0;
+    m_cost = 0.00;
+    m_IsCheckOut = false;
+}
+Book::Book(int id, string title, string author, int num_of_pages, double cost, bool isCheckOut)
+{
+    m_id = id;
+    m_title = title;
+    m_author = author;
+    m_num_of_pages = num_of_pages;
+    m_cost = cost;
+    m_IsCheckOut = isCheckOut;
+}
